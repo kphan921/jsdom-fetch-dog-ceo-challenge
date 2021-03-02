e65392284ab4d48200b7d8ff508a9bec1401815b
Tri <triphan921@gmail.com>
2021-03-01 21:38:49 -0800

    commit tree 1b643ffba28b10e542d024aee4d81d699e3ce31d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', ()=> {
+    // function addImages() {
+    // fetch('https://dog.ceo/api/breeds/image/random/4')
+    // .then(resp => resp.json())
+    // .then(obj => obj.message.forEach(dog=>renderOneDog(dog)));
+    // };
+
+    // function renderOneDog(dog) {
+    // const dogDiv = document.createElement('div');
+    // dogDiv.className = "dog-image";
+    // dogDiv.innerHTML = `
+    // <img src="${dog}" alt='image of dog'></img>
+    // `
+    // document.querySelector('#dog-image-container').appendChild(dogDiv)
+    // }
+
+    // addImages();
+
+    const breedSelect = document.querySelector('#breed-dropdown');
+    breedSelect.addEventListener('change', (e)=> {
+        const letter = e.target.value;
+        document.querySelector('#dog-breeds').innerHTML = '';
+
+        function addBreeds() {
+            fetch('https://dog.ceo/api/breeds/list/all')
+            .then(resp => resp.json())
+            .then(obj => Object.keys(obj.message)
+                .filter(dog=> dog[0] === letter)
+                .forEach(dog=>renderBreed(dog))
+            );
+        };
+    
+        function renderBreed(dog) {
+            const dogLi = document.createElement('li');
+            dogLi.textContent = dog;
+            dogLi.addEventListener('click', ()=>{
+                dogLi.style.color = 'red';
+            })
+        
+            
+            document.querySelector('#dog-breeds').appendChild(dogLi)
+        };
+        addBreeds();
+    });
+});
